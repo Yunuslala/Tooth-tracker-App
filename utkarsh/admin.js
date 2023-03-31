d03d76530ed381651497094f897d80af22904dec
utdsi <utkarshsinha852@gmail.com>
2023-04-01 00:36:29 +0530

    doctor add

--- utkarsh/admin.js
+++ utkarsh/admin.js
@@ -66,3 +66,73 @@ async function getDoctors() {
 
     })
 }
+
+document.querySelector(".slots").addEventListener("click", () => {
+    getSlots();
+})
+
+async function getSlots() {
+    let slots = await fetch(`${baseURL}/admin/query`, {
+        method: 'GET',
+        headers: {
+            'content-type': 'application/json',
+            'authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTcsIm5hbWUiOiJhZG1pbjEiLCJwaG9uZSI6IjEyMzQ1Njc4OTAiLCJlbWFpbCI6ImFkbWluMUBnbWFpbC5jb20iLCJwYXNzd29yZCI6IiQyYiQwNyRxY2lQS3FWZFFRRFd4Lno0VGlKb2hlejc5eXpVZ3hYejRnV0tCU3VJVHo1Y3lvVDNlS292cSIsInJvbGUiOiJhZG1pbiIsImRhdGVfb2ZfYmlydGgiOiIyMDAzLTA4LTEyVDAwOjAwOjAwLjAwMFoiLCJHaXRodWIiOm51bGwsImlhdCI6MTY4MDI4NDE4OH0.t6IyQ9lqngiaaHXSZivHpNRtGkgwQqzDq8lijjnS58c'
+        },
+        body:{"query":"select * from slots"}
+
+    })
+    slot = await slots.json();
+    console.log(slot);
+    // let div = document.getElementById("user")
+    // div.style.display = "block"
+
+    // users.map((item) => {
+
+    //     let img = document.createElement("img")
+    //     img.src = "./user.jpg"
+    //     let h1 = document.createElement("h1")
+    //     h1.innerHTML = item.name
+
+    //     div.append(img, h1)
+
+
+    // })
+}
+
+
+let form1 = document.getElementById("f1")
+
+let s1 = document.getElementById("s1")
+
+s1.onclick = (event)=>{
+event.preventDefault()
+
+let obj ={
+    name:form1.dname.value,
+    speciality:form1.spec.value,
+    sub_speciality:form1.sub.value,
+    degree:form1.deg.value
+}
+postDoctors(obj)
+
+//console.log(obj)
+
+}
+
+
+async function postDoctors(obj){
+
+    let data = await fetch(`${baseURL}/admin/addDoctor`,{
+        method:"POST",
+        headers: {
+            'content-type': 'application/json',
+            'authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTcsIm5hbWUiOiJhZG1pbjEiLCJwaG9uZSI6IjEyMzQ1Njc4OTAiLCJlbWFpbCI6ImFkbWluMUBnbWFpbC5jb20iLCJwYXNzd29yZCI6IiQyYiQwNyRxY2lQS3FWZFFRRFd4Lno0VGlKb2hlejc5eXpVZ3hYejRnV0tCU3VJVHo1Y3lvVDNlS292cSIsInJvbGUiOiJhZG1pbiIsImRhdGVfb2ZfYmlydGgiOiIyMDAzLTA4LTEyVDAwOjAwOjAwLjAwMFoiLCJHaXRodWIiOm51bGwsImlhdCI6MTY4MDI4NDE4OH0.t6IyQ9lqngiaaHXSZivHpNRtGkgwQqzDq8lijjnS58c'
+        },
+        body:JSON.stringify(obj)
+
+    })
+
+    console.log(obj)
+
+}
+
