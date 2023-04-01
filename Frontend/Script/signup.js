a21534171223ffb9c04740ad8175627fd0585183
Yunuslala <saifjava2@gmail.com>
2023-04-02 04:40:59 +0530

    integreation completed

---
+++ Frontend/Script/signup.js
@@ -0,0 +1,41 @@
+document.querySelector("#register").addEventListener("submit",async (event) => {
+    event.preventDefault();
+    const name = document.querySelector("#name").value;
+    const email = document.querySelector("#email").value;
+    const phone = document.querySelector("#phone").value;
+    const password = document.querySelector("#password").value;
+    const date_of_birth = document.querySelector("#date_of_birth").value;
+
+    const payload = {
+        name,
+        email,
+        phone,
+        password,
+        date_of_birth
+    }
+
+    console.log(payload);
+
+    try {
+        let res = await fetch('https://tooth-tracker.cyclic.app/register',{
+            method:'POST',
+            headers:{
+                'content-type':'application/json'
+            },
+            body: JSON.stringify(payload)
+        });
+
+        res = await res.json();
+        console.log(res);
+        alert(res.msg);
+        
+    }
+    catch (error) {
+        console.log(error);
+    }
+
+});
+
+function home(){
+    window.location.href="../index.html"
+  }
--- Yunus/test.js
+++
@@ -1,6 +0,0 @@
-const ScheduleSystem=require("./app").ScheduleSystem;
-
-let system=new ScheduleSystem();
-
-let ans=system.initializeUser("raushan","21","r@gmail.com",9595995);
-console.log(ans);
